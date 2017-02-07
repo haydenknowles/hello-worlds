629aed3b1f883307d15ce9aee91aa25cc56117b6
Hayden Knowles <hayden.knowles@live.com>
2017-02-07 22:23:19 +1300

    fixed port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var http = require('http')
 
 server = http.createServer(function(request, response) {
-  request.write('Hello World')
-  request.close()
+  response.write('Hello World')
+  response.close()
 })
 
 server.listen(3000)
