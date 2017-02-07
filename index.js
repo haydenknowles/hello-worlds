c569baeea3e8271cfc594013b6fecb616cf6ee01
Hayden Knowles <hayden.knowles@live.com>
2017-02-07 22:03:58 +1300

    testing

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-http = require('http')
+var express = require('express');
+var app = express();
 
-server = http.createServer(function(request, response) {
-		response.write('Hello World!')
-		response.end()
-})
+app.set('port', (process.env.PORT || 5000));
 
-server.listen(3002)
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
