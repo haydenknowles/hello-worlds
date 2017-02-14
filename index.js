d80db0f514f4e1edb485043ce419d069437f310f
Hayden Knowles <hayden.knowles@live.com>
2017-02-14 21:55:32 +1300

    testing postgres

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var http = require('http')
 
+const pg = require('pg');
+const connString = 'postgres://localhost:5432/haydenknowles'
+
+pg.connect(connString, (err, client, done) => {
+  if (err) return console.error('error fetching client from pool', err);
+  client.query('SELECT $1::varchar AS my_first_query', ['haydenknowles'], function (err, result) {
+      done()
+      if (err) console.error('error happened during query', err);
+  })
+}))
 var server = http.createServer(function(request, response) {
   response.write('Hello World')
   console.log('got a request!')
