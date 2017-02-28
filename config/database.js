e15674f4cf858cf34d3f3cfa9b903028482543e6
Hayden Knowles <hayden.knowles@live.com>
2017-03-01 00:13:54 +1300

    connects to heroku database

--- config/database.js
+++ config/database.js
@@ -1,16 +1,24 @@
 const pg = require('pg')
 
 // conn strings
-const connString = 'postgres://gvodabrblszwoy:b15ed1f7d7bc2630f57c3fc0d6eba497369873e673692119034e909f13fc3163@ec2-54-225-122-119.compute-1.amazonaws.com:5432/d8dpmerp72t1cu'
-const localString = 'postgres://localhost:5432/haydenknowles'
+const connString = 'postgres://@ec2-54-225-122-119.compute-1.amazonaws.com:5432/d8dpmerp72t1cu '
+const connection = new pg.Client({
+  user: "gvodabrblszwoy",
+  password: "password",
+  database: "d8dpmerp72t1cu",
+  port: "5432",
+  host: "ec2-54-225-122-119.compute-1.amazonaws.com",
+  ssl: true
+})
+
+//const localString = 'postgres://localhost:5432/haydenknowles'
 
 function pgConnection(callback){
 
 }
+module.exports = {}
 
-pg.connect(connString, (err, client, done) => {
-  module.exports = {
-    client: client,
-    done: done
-  }
+connection.connect((err, client, done) => {
+  module.exports.client = client
+  module.exports.done = done
 })
--- index.js
+++ index.js
@@ -3,46 +3,19 @@ const http = require('http');
 const pg = require('pg');
 const express = require('express');
 const path = require('path');
-const client = require('./config/database').database;
+const client = require('./config/database.js');
 
 
 var res;
 const app = express()
-
-app.engine('.hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs',
-  layoutDir: path.join(__dirname, 'views/layouts')
-}))
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, 'views'))
-
 app.get('/', (request, response) => {
-  // pg.connect(connString, (err, client, done) => {
-  //   if (err) return console.error('error fetching client from pool', err);
-  //   client.query('SELECT * from "public"."nodetest" where id = $1', ['2'], function (err, result){
-  //       done()
-  //       if (err) console.error('error happened during query', err);
-  //       //console.log(typeof result.rows[0].id);
-  //       res = result.rows[0]
-  //       //process.exit(0);
-  //
-  //       response.render('home', {
-  //         name: res.name
-  //       })
-  //   })
-
-  //})
-client.query('SELECT * from "public"."nodetest" where id = $1', ['2'], function (err, result){
-      done()
+  client.client.query('SELECT * from "public"."Languages" where id = $1', ['100'], function (err, result){
       if (err) console.error('error happened during query', err);
-      //console.log(typeof result.rows[0].id);
       res = result.rows[0]
-      //process.exit(0);
-      console.log(res.name);
-      response.write("Hello " + res.name)
+      response.send(res.language + "<br />" + res.code)
   })
 })
 var port = process.env.PORT || 3000
+
 app.listen(port)
 console.log('Server is listening for requests...')
