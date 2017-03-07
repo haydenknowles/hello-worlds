997dd857dd7436889a9d2d5ea973d49f611a3dbd
haydenknowles <hayden.knowles@live.com>
2017-03-07 09:44:21 +0000

    fixed

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const pg = require('pg')
 const connString = 'postgres://@ec2-54-225-122-119.compute-1.amazonaws.com:5432/d8dpmerp72t1cu '
 const connection = new pg.Client({
   user: "gvodabrblszwoy",
-  b15ed1f7d7bc2630f57c3fc0d6eba497369873e673692119034e909f13fc3163: "b15ed1f7d7bc2630f57c3fc0d6eba497369873e673692119034e909f13fc3163",
+  password: "b15ed1f7d7bc2630f57c3fc0d6eba497369873e673692119034e909f13fc3163",
   database: "d8dpmerp72t1cu",
   port: "5432",
   host: "ec2-54-225-122-119.compute-1.amazonaws.com",
