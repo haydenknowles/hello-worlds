1159b04d12b74f380f2e28fa312e0f8bc918a89c
Ricky Dua <rickx342@gmail.com>
2017-04-11 22:48:30 +1200

    initial search working

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,19 @@
 
 var bar_search = document.getElementById("search_bar");
-bar_search.onkeyup = fucntion
+
+function search(){
+	var searchValue = bar_search.value.toLowerCase();
+	var languages = document.getElementsByClassName("language_code");
+
+	for(var i = 0; i<languages.length;i++){
+		var language = languages[i].getElementsByTagName("h3")[0];
+		if(language.innerText.toLowerCase().indexOf(searchValue) > -1){
+			languages[i].style.display = "block"
+		}
+		else{
+			languages[i].style.display = "none"
+		}
+	}
+}
+
+bar_search.onkeyup = search;
